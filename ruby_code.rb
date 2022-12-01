550bea602a59be88a562cbefdaf1f33ee2ae3b04
1millionoranges <22225197+1millionoranges@users.noreply.github.com>
2022-11-30 20:22:57 -0600

    Add user input and game loop. Debug get winner methods.

--- ruby_code.rb
+++ ruby_code.rb
@@ -8,9 +8,9 @@ class Board
         puts "printing board"
         @board_state.each do |row|
             row.each do |spot|
-                print spot.to_s.concat("|")
+                print spot.to_s + ("   ")
             end
-            print "\n-+-+-\n"
+            print "\n\n"
         end
     end
     def add_move(row, column, player)
@@ -19,14 +19,16 @@ class Board
 
 
     def check_horizontal_winner
+        winner = false
         @board_state.each do |row|
-            return (row.uniq.length == 1)
+            winner = true if (row.uniq.length == 1) && (row[0] != 0)
         end
+        return winner
     end
     def check_vertical_winner
         for i in (0..@size-1) do
-            winner = true
             top_value = @board_state[0][i]
+            winner = top_value != 0
             for j in (1..@size-1) do
                 winner = false if(@board_state[j][i] != top_value)
             end
@@ -36,9 +38,9 @@ class Board
     end
     def check_diagonal_winner
         top_left = @board_state[0][0]
-        top_left_winner = true
+        top_left_winner = top_left != 0
         bottom_left = @board_state[0][@size - 1]
-        bottom_left_winner = true
+        bottom_left_winner = bottom_left != 0
         @board_state.each_index do |i|
             top_left_winner = false if(@board_state[i][i] != top_left)
             bottom_left_winner = false if(@board_state[-i][i] != bottom_left)
@@ -49,9 +51,27 @@ class Board
     def check_winner
         return (check_diagonal_winner || check_horizontal_winner || check_vertical_winner)
     end
-                
+    
+    def get_move(player)
+        puts "\nPlease enter your move"
+        move_string = gets.chomp
+        move_arr = move_string.split(",")
+        move_x_pos = move_arr[0].to_i
+        move_y_pos = move_arr[1].to_i
+        add_move(move_x_pos, move_y_pos, player)
+    end
+
+    def game_loop
+        
+        while (!check_winner) do 
+            print_board
+            get_move("X")
+        end
+        print_board
+        puts "\n\n\nWe have a winner!"
+    end
 end
 
 my_board = Board.new(3)
 
-my_board.print_board
+my_board.game_loop
